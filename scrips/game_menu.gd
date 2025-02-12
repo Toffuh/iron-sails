a60cf56c37e09b9fb30c73c286891c01d8572944
benni <benni.huff@gmail.com>
2025-02-12 17:33:45 +0100

    changes

--- scrips/game_menu.gd
+++ scrips/game_menu.gd
@@ -2,19 +2,16 @@ extends Control
 
 var menu_visible = false
 
-@onready var menu = $"."
-@onready var button = $Button
-
 func _ready():
-	menu.visible = menu_visible
+	visible = menu_visible
 
 func _process(delta):
-	if Input.is_action_just_pressed("game_menu"):  # "F" wurde gedrückt
+	if Input.is_action_just_pressed("game_menu"):
 		toggle_menu()
 
 func toggle_menu():
 	menu_visible = !menu_visible
-	menu.visible = menu_visible
+	visible = menu_visible
 
 func _on_button_pressed() -> void:
 	print("Button wurde gedrückt!")
