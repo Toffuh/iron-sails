ceb823d6544f60d9bfd5c8b39eae48ba63448af2
benni <benni.huff@gmail.com>
2025-02-12 17:34:24 +0100

    stuff

--- scrips/game_menu.gd
+++ scrips/game_menu.gd
@@ -1,17 +1,21 @@
 extends Control
 
-var menu_visible = false
+var menu_visible: bool = false
+
 
 func _ready():
 	visible = menu_visible
 
-func _process(delta):
+
+func _process(_delta):
 	if Input.is_action_just_pressed("game_menu"):
 		toggle_menu()
+
 
 func toggle_menu():
 	menu_visible = !menu_visible
 	visible = menu_visible
+
 
 func _on_button_pressed() -> void:
 	print("Button wurde gedrückt!")
